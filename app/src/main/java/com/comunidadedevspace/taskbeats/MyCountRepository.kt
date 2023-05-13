23712d33de02a4ea9ae9ab1524f3c2543664cc30
pedrocerqueiras <pedrosilvap2@hotmailcom>
2023-05-13 19:51:08 -0300

    app bottom bar and fragments

---
+++ app/src/main/java/com/comunidadedevspace/taskbeats/MyCountRepository.kt
@@ -0,0 +1,37 @@
+package com.comunidadedevspace.taskbeats
+
+import kotlin.random.Random
+
+interface MyCountRepository {
+
+    fun sum(): Int
+
+    fun sub (p1: Int, p2: Int): Int
+
+}
+
+class MyCountRepositoryImpl(private val numbersProvider: MyNumbersProvider): MyCountRepository {
+
+    override fun sum(): Int {
+        val p1= numbersProvider.getNumber()
+        val p2= numbersProvider.getNumber()
+        return p1 + p2
+    }
+
+
+
+    override fun sub(p1: Int, p2: Int): Int = p1 - p2
+
+}
+
+interface MyNumbersProvider {
+
+    fun getNumber():Int
+    
+}
+
+class MyNumbersProviderImpl: MyNumbersProvider {
+    override fun getNumber(): Int {
+       return Random.nextInt(from = 0 , until = 100)
+    }
+}
--- app/src/test/java/com/comunidadedevspace/taskbeats/ExampleUnitTest.kt
+++ app/src/test/java/com/comunidadedevspace/taskbeats/ExampleUnitTest.kt
@@ -2,7 +2,9 @@ package com.comunidadedevspace.taskbeats
 
 import org.junit.Test
 
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +12,26 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+    private val mockMyNumbersProvider: MyNumbersProvider = mock()
+
+    private val underTest = MyCountRepositoryImpl (
+        numbersProvider = mockMyNumbersProvider
+            )
+
+
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+
+        //Given
+        whenever(mockMyNumbersProvider.getNumber()).thenReturn(2)
+
+        //When
+        val result = underTest.sum()
+
+        //Then
+        val expected = 4
+        assertEquals(expected, result)
+
     }
 }
